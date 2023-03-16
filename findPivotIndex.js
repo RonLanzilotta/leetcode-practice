7c3cd4c697aec8443849ad893c9d60c73274c4ff
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-03-16 15:46:44 -0400

    cleaned up code

--- findPivotIndex.js
+++ findPivotIndex.js
@@ -4,15 +4,8 @@ function findPivot(arr) {
 
     let leftSum = 0; 
     let rightSum = 0;
-    // let pivotIndex = 0;
-    // let pivotIndex = Math.floor(arr.length / 2);
-
-    // while(0 <= pivotIndex < arr.length ){
-        // console.log(`current pivot index: ${pivotIndex}`)
-
+    
     for (let pivotIndex = 0; pivotIndex < arr.length; pivotIndex++){
-        console.log(pivotIndex)
-        // breaks arr into leftSum + rightSum based on 
         for (let i = 0; i < arr.length; i++) {
 
             if (i < pivotIndex) {
@@ -23,18 +16,16 @@ function findPivot(arr) {
                 leftSum = 0;
             } else if (pivotIndex === arr.length - 1) {
                 rightSum = 0;
-            } console.log('end loop 1')
+            } 
         }
 
         if (leftSum == rightSum) { 
-            console.log('success')
             return pivotIndex
         } else {
             leftSum = 0
             rightSum = 0
         }
     } 
-    console.log('fail') 
     return -1
 }
 
