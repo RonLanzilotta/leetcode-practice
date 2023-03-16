411358d6ef66ab571ecfa433bd73dd7f1960cfcb
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-03-16 15:46:05 -0400

    all example cases are working

--- findPivotIndex.js
+++ findPivotIndex.js
@@ -1,48 +1,41 @@
-let input = [1,7,3,6,5,6,7]
-
-// function iterator(leftSum, rightSum, num) {
-//     if (leftSum < rightSum) { num =+ 1 } 
-//     if (leftSum > rightSum) { num =- 1 }
-// }
+let input = [2, 1, -1]
 
 function findPivot(arr) {
 
     let leftSum = 0; 
     let rightSum = 0;
+    // let pivotIndex = 0;
+    // let pivotIndex = Math.floor(arr.length / 2);
 
-    for (let pivotIndex = Math.floor(arr.length / 2); 0 <= pivotIndex < arr.length; pivotIndex++) {
+    // while(0 <= pivotIndex < arr.length ){
+        // console.log(`current pivot index: ${pivotIndex}`)
 
+    for (let pivotIndex = 0; pivotIndex < arr.length; pivotIndex++){
+        console.log(pivotIndex)
+        // breaks arr into leftSum + rightSum based on 
         for (let i = 0; i < arr.length; i++) {
 
             if (i < pivotIndex) {
                 leftSum += arr[i]
-            }
-            
-            if (i > pivotIndex) {
+            } else if (i > pivotIndex) {
                 rightSum += arr[i]
-
             } else if (pivotIndex === 0) {
                 leftSum = 0;
-
             } else if (pivotIndex === arr.length - 1) {
                 rightSum = 0;
-
-            }
+            } console.log('end loop 1')
         }
 
         if (leftSum == rightSum) { 
+            console.log('success')
             return pivotIndex
-
-        } else if (leftSum < rightSum) {
-            pivotIndex++
-
-        } else if (leftSum > rightSum) {
-            pivotIndex--
-            
-        } else { 
-            return -1 
+        } else {
+            leftSum = 0
+            rightSum = 0
         }
-    }
+    } 
+    console.log('fail') 
+    return -1
 }
 
 console.log(findPivot(input))
