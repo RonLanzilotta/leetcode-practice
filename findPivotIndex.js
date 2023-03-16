2609403245a0891da488ccc0d9a7fb679be350d2
Ron Lanzilotta <rlanzilotta1010@gmail.com>
2023-03-16 14:36:34 -0400

    attempting to do away with iterator function in parent for loop

--- findPivotIndex.js
+++ findPivotIndex.js
@@ -1,21 +1,48 @@
-let input = [1, 7, 3, 6, 5, 6]
+let input = [1,7,3,6,5,6,7]
+
+// function iterator(leftSum, rightSum, num) {
+//     if (leftSum < rightSum) { num =+ 1 } 
+//     if (leftSum > rightSum) { num =- 1 }
+// }
 
 function findPivot(arr) {
-    let leftSum = []; 
-    let rightSum = [];
-    let pivotIndex = Math.floor(arr.length / 2);
 
-    for (let i = 0; i < arr.length; i++) {
-        if (i < pivotIndex) {
-            leftSum.push(arr[i]) 
-        } else if (i > pivotIndex) {
-            rightSum.push(arr[i])
-        } else {
-            continue
+    let leftSum = 0; 
+    let rightSum = 0;
+
+    for (let pivotIndex = Math.floor(arr.length / 2); 0 <= pivotIndex < arr.length; pivotIndex++) {
+
+        for (let i = 0; i < arr.length; i++) {
+
+            if (i < pivotIndex) {
+                leftSum += arr[i]
+            }
+            
+            if (i > pivotIndex) {
+                rightSum += arr[i]
+
+            } else if (pivotIndex === 0) {
+                leftSum = 0;
+
+            } else if (pivotIndex === arr.length - 1) {
+                rightSum = 0;
+
+            }
+        }
+
+        if (leftSum == rightSum) { 
+            return pivotIndex
+
+        } else if (leftSum < rightSum) {
+            pivotIndex++
+
+        } else if (leftSum > rightSum) {
+            pivotIndex--
+            
+        } else { 
+            return -1 
         }
     }
-    console.log(leftSum)
-    console.log(rightSum)
 }
 
-findPivot(input)
+console.log(findPivot(input))
